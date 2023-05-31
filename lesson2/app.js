b9da35562a7a7a7c59d71aee641e14b9e6b17e4a
Datek1 <yurarudkevich81@gmail.com>
2023-06-01 02:31:58 +0300

    upgrage

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const users = require('./users.json')
+const fs = require('node:fs')
+const {json} = require("express");
+
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
 
 const port = 4000
 
@@ -11,8 +14,9 @@ app.get('/users', (req, res) => {
     res.status(200).json(users)
 })
 
-app.post('/users' ,(req,res)=>{
-    console.log(req.body);
+app.post('/users', (req, res) => {
+    fs.writeFile('users.json',`${JSON.stringify({"results":[...users.results, req.body]})}`,(err)=>{})
+    res.status(201).json({message:'User Created'})
 })
 
 app.listen(port, () => {
