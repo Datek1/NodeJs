9d30c9b5177f62014244ffeccfc427baf9f50854
Datek1 <yurarudkevich81@gmail.com>
2023-06-01 18:48:51 +0300

    lesoon2

--- lesson2/app.js
+++ lesson2/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const users = require('./users.json')
 const fs = require('node:fs')
-const {json} = require("express");
+
 
 
 app.use(express.json())
@@ -15,10 +15,41 @@ app.get('/users', (req, res) => {
 })
 
 app.post('/users', (req, res) => {
-    fs.writeFile('users.json',`${JSON.stringify({"results":[...users.results, req.body]})}`,(err)=>{})
-    res.status(201).json({message:'User Created'})
+    if (req.body.name.length >= 3 && req.body.age >= 0) {
+        fs.writeFile('users.json', `${JSON.stringify({"results": [...users.results, req.body]})}`, (err) => {
+        })
+        res.status(201).json({message: 'User Created'})
+    } else {
+        throw new Error('Name - min lenght 3 and age > 0 ')
+    }
+
 })
 
+app.put('/users/:id', (req, res) => {
+    const {id} = req.params
+    const user = users.results[+id]
+    if (!user) {
+        throw new Error('User isn`t finded')
+    } else {
+        res.status(200).json({message: 'User updated'})
+        users.results[+id] = req.body
+        fs.writeFile('users.json', `${JSON.stringify((users))}`, (err) => {
+        })
+    }
+})
+
+app.delete('/users/:id', (req, res) => {
+    const {id} = req.params
+    const user = users.results[+id]
+    if(!user){
+        throw new Error('Users isn`t finded')
+    }else {
+        res.status(200).json({message:'User deleted'})
+        users.results.splice(+id,1)
+        fs.writeFile('users.json', `${JSON.stringify((users))}`, (err) => {
+        })
+    }
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
